2c2341188d1dcabbc3383aae135161b10e549f87
SAaASs <118337995+SAaASs@users.noreply.github.com>
2022-11-23 12:09:39 +0300

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,36 @@
+def merge_sort(alist, start, end):
+    if end - start > 1:
+        mid = (start + end)//2
+        merge_sort(alist, start, mid)
+        merge_sort(alist, mid, end)
+        merge(alist, start, mid, end)
+ 
+def merge(alist, start, mid, end):
+    left = alist[start:mid]
+    right = alist[mid:end]
+    k = start
+    i = 0
+    j = 0
+    while (start + i < mid and mid + j < end):
+        if (left[i] <= right[j]):
+            alist[k] = left[i]
+            i = i + 1
+        else:
+            alist[k] = right[j]
+            j = j + 1
+        k = k + 1
+    if start + i < mid:
+        while k < end:
+            alist[k] = left[i]
+            i = i + 1
+            k = k + 1
+    else:
+        while k < end:
+            alist[k] = right[j]
+            j = j + 1
+            k = k + 1
+ 
+ 
+alist = [9,8,7,6,5,4,3,2,1]
+merge_sort(alist, 0, len(alist))
+print(alist)
