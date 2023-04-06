43e9b0958e84f205e6b779471db6d7aef9ab3b78
SAaASs <118337995+SAaASs@users.noreply.github.com>
2023-04-06 22:06:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
-from aiogram import Bot, Dispatcher, types, executor
-from datetime import datetime, date, time
+import asyncio
+import logging
+from aiogram import Bot,Dispatcher
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from datetime import datetime, date, time, timedelta
 import codecs
-file = codecs.open( "Test.txt", "r", "utf-8" )
-data = file.readlines()
-nov = datetime.now()
-TOKEN_API = "6087945888:AAFRDGzwQ9O1fFE3E74FtEAx84QY0WQDiHc"
-bot = Bot(TOKEN_API)
-dp = Dispatcher(bot)
-test_chat = -1001956903015
 import threading
+file = codecs.open( "Test.txt", "r", "utf-8" )
+Students = file.readlines()
+scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 
-def f():
-  threading.Timer(5.0, f).start()  # Перезапуск через 5 секунд
-  for i in range(0,len(data)):
-    data1 = data[i].split()
-    if datetime(nov.year,nov.month,nov.day).day == datetime(int(data1[4]),int(data1[3]),int(data1[2])).day and datetime(nov.year,nov.month,nov.day).month == datetime(int(data1[4]),int(data1[3]),int(data1[2])).month:
-        name = data1[0]
-        fam = data1[1]
-        tele_id = data1[5]
-        @dp.message_handler(commands=["ok"])
-        async def send_congratulation(msg: types.Message):
-            await bot.send_message(tele_id, 'Поздравляем {0} {1} с днем рождения'.format(name, fam))
+nov = datetime.now()
+logging.basicConfig(level=logging.INFO)
+token = "6087945888:AAFRDGzwQ9O1fFE3E74FtEAx84QY0WQDiHc"
+bot = Bot(token)
+dp = Dispatcher(bot)
 
-f()
+today_congrats = []
+def check_date():
+    for i in range(0,len(Students)):
+        Student_line = Students[i].split(" ")
+        if int(Student_line[2]) == nov.day and int(Student_line[3]) == nov.month:
+            today_congrats.append('Поздравляем {0} {1} с днем рождения'.format(Student_line[0], Student_line[1]))
+async def cmd_start(bot: Bot):
+    for i in range(0, len(today_congrats)):
+        await bot.send_message(487539481,today_congrats[i])
+scheduler.add_job(check_date, "cron", hour = 10, args=(bot,))
+scheduler.add_job(cmd_start, "cron", hour = 10, args=(bot,))
 
 
-@dp.message_handler(commands=['start'])
-async def start_talk(message: types.Message):
-    await message.reply("Стартовое сообщение")
 
-#@dp.message_handler()
-#async def echo_message(msg: types.Message):
-#    await bot.send_message(-1001956903015, msg.text)
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
+async def main():
+    scheduler.start()
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
