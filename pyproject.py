bb9b62c660cbb6e901c9816d7bc9bde34c59511f
SAaASs <118337995+SAaASs@users.noreply.github.com>
2023-05-04 12:34:53 +0300

    Update pyproject.py

--- pyproject.py
+++ pyproject.py
@@ -7,13 +7,10 @@ temperature_list = []
 headers_ = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 data = requests.get(url, headers=headers_)
 data_ =bs(data.content, 'html.parser')
-print(data_)
-thedata = str(data_)
-ind1 = thedata.find(":[{")
-ind2 = thedata.find("]}")
-thedata = thedata[ind1:ind2]
+newdata = data_.find("script", id="charts44235805")
 billions_of_players = []
-numbers = re.findall(r'\d+(?:\.\d+)?', thedata)
-for i in range(0,len(numbers),3):
-    billions_of_players.append(numbers[i])
+newdata = str(newdata)
+numbers = re.findall(r'\d+(?:\.\d+)?', newdata)
+for i in range(4,len(numbers), 3):
+    billions_of_players.append(float(numbers[i]))
 print(billions_of_players)
