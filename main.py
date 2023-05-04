d24d21c89bdc4443d9adf52b5aea08d7274f6ade
SAaASs <118337995+SAaASs@users.noreply.github.com>
2023-05-04 09:07:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,19 @@
-import asyncio
-import logging
-from aiogram import Bot,Dispatcher
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import datetime, date, time, timedelta
-import codecs
-import threading
-file = codecs.open( "Test.txt", "r", "utf-8" )
-Students = file.readlines()
-scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
-
-nov = datetime.now()
-logging.basicConfig(level=logging.INFO)
-token = "6087945888:AAFRDGzwQ9O1fFE3E74FtEAx84QY0WQDiHc"
-bot = Bot(token)
-dp = Dispatcher(bot)
-
-today_congrats = []
-def check_date():
-    for i in range(0,len(Students)):
-        Student_line = Students[i].split(" ")
-        if int(Student_line[2]) == nov.day and int(Student_line[3]) == nov.month:
-            today_congrats.append('Поздравляем {0} {1} с днем рождения'.format(Student_line[0], Student_line[1]))
-async def cmd_start(bot: Bot):
-    for i in range(0, len(today_congrats)):
-        await bot.send_message(487539481,today_congrats[i])
-scheduler.add_job(check_date, "cron", hour = 10, args=(bot,))
-scheduler.add_job(cmd_start, "cron", hour = 10, args=(bot,))
-
-
-
-
-async def main():
-    scheduler.start()
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
-
+import numpy as np
+import requests
+import re
+from  bs4 import BeautifulSoup as bs
+url = "https://financesonline.com/number-of-gamers-worldwide/"
+temperature_list = []
+headers_ = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+data = requests.get(url, headers=headers_)
+data_ =bs(data.content, 'html.parser')
+print(data_)
+thedata = str(data_)
+ind1 = thedata.find(":[{")
+ind2 = thedata.find("]}")
+thedata = thedata[ind1:ind2]
+billions_of_players = []
+numbers = re.findall(r'\d+(?:\.\d+)?', thedata)
+for i in range(0,len(numbers),3):
+    billions_of_players.append(numbers[i])
+print(billions_of_players)
