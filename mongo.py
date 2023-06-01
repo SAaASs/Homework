c16de68b7c97e75d0c740159564b875ff7b8650a
SAaASs <118337995+SAaASs@users.noreply.github.com>
2023-06-01 13:07:57 +0300

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-import pprint
+import random
 from pymongo.errors import DuplicateKeyError
 
 # mongodb://user:pass@host/Database - если к внешнему хосту, если localhost, то ничего передавать не надо
@@ -24,16 +24,17 @@ def initDB():
     queries.drop()
     for i in range(5):
         insert_in(comps, comps_mass[i])
+        print("В список компаний добавлена компания", comps_mass[i])
         insert_in(places, places_mass[i])
+        print("В список мест добавлено место", places_mass[i])
 def piss(target_name):
     cursor = comps.find({"name": "{0}".format(target_name)})
     for cur in cursor:
         print(cur)
-initDB()
 def take_money(target_name, money_amount):
-    cursor = comps.find({"name": "{0}".format(target_name)})
-    if float(cursor[0]["money_amount"]) - float(money_amount)>0:
-        target_money_amount = float(cursor[0]["money_amount"]) - float(money_amount)
+    cursor = comps.find_one({"name": "{0}".format(target_name)})
+    if float(cursor["money_amount"]) - float(money_amount)>0:
+        target_money_amount = float(cursor["money_amount"]) - float(money_amount)
     else:
         print("Ошибкастоп ноль0ноль0ноль0ноль0ноль0ноль0ноль0ноль0ноль0")
         print("Недостаточно денег на сете компании {0}".format(target_name))
@@ -45,15 +46,24 @@ def take_money(target_name, money_amount):
     return 1
 
 def create_query(target_place):
-    cursor = places.find({"name": "{0}".format(target_place)})
-    ccomp=cursor[0]["cont_comp"]
-    price=cursor[0]["price"]
-    value = cursor[0]["value"]
-    x = take_money(ccomp, price)
-    if x==1:
-        insert_in(queries,{"place":target_place,"comp":ccomp,"check":price,"value":value})
+    cursor = places.find_one({"name": "{0}".format(target_place)})
+    if cursor is None:
+        print("Создание заказа прервано. Причина: место {0} не существует".format(target_place))
     else:
-        return 0
+        ccomp=cursor["cont_comp"]
+        price=cursor["price"]
+        value = cursor["value"]
+        x = take_money(ccomp, price)
+        if x==1:
+            insert_in(queries,{"place":target_place,"comp":ccomp,"check":price,"value":value})
+            print("Запрос на упорку места {0}, объемом {1} м^3, компанией {2}, на сумму {3} рублIв принят и скоро будет исполнен, ожидайте".format(target_place,value,ccomp,price))
+        else:
+            print("заказ отменен")
+            return 0
+def makeJob(operation_amount):
+    for i in range(0, operation_amount):
+        create_query(places_mass[random.randint(0,5)]["name"])
 
 
-create_query("place1")
+initDB()
+makeJob(15)
